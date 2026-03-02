9f9f7d207eaca34a75fdd610480f4629cc5eced2
cfunch29 <cfunch29@gmail.com>
2026-03-02 15:54:14 -0500

    add middleware and routes folder, setup server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+// imports 
+import express from "express";
+
+// data 
+
+
+// setups 
+const app = express();
+const PORT = 3000;
+
+// middleware
+
+// routes 
+
+// global err handling middleware 
+
+
+// listener
