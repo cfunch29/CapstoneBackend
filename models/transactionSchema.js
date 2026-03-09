1ea9304e1b1a27d3ffe96b018b22170e7090fc2d
cfunch29 <cfunch29@gmail.com>
2026-03-09 13:41:39 -0400

    added transaction schema model

---
+++ models/transactionSchema.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const TransactionSchema = new mongoose.Schema({
+_id: { 
+type: String,
+required: true,
+},
+ 
+amount: {
+type: number,
+required: true,
+},
+
+type: {
+type: String,
+},
+ 
+category: {
+type: String,
+},
+
+description: {
+type: String,
+},
+
+date: {
+type: String,
+required: true,
+},
+
+});
+
+const Transaction = mongoose.model('transaction', TransactionSchema)
+
+export default Transaction;
